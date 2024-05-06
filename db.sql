2a9f205422749377b38737f9c67ee8217b0af0c7
daizeyao <1192313493@qq.com>
2024-05-06 23:38:43 +0800

    feat: 数据库文件更新

--- db.sql
+++ db.sql
@@ -1,14 +1,38 @@
-DROP DATABASE IF EXISTS fileencode;
+/*
+ Navicat Premium Data Transfer
 
-CREATE DATABASE fileencode;
+ Source Server         : dzy521
+ Source Server Type    : MySQL
+ Source Server Version : 80032
+ Source Host           : localhost:3306
+ Source Schema         : fileencode
 
-USE fileencode;
+ Target Server Type    : MySQL
+ Target Server Version : 80032
+ File Encoding         : 65001
 
-DROP TABLE IF EXISTS users;
+ Date: 06/05/2024 23:31:36
+*/
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    salt VARCHAR(255) NOT NULL,
-    hashed_password VARCHAR(255) NOT NULL
-);
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `salt` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `hashed_password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `role` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES ('admin', '', 'eaa5c84a6c43301d0eebb96ba9d99cba', '381dd7a62fb29e2a5e9d62f2e8b1aa5f4f312b164c9956a857671fa0e823f101', 'admin');
+
+SET FOREIGN_KEY_CHECKS = 1;
